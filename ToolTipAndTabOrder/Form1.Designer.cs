ddf24d67b98533506711dd28924d60d2124ad96d
KGRAVES <KGRAVES@blueridgectc.edu>
2024-04-01 21:17:50 -0400

    Add project files.

---
+++ ToolTipAndTabOrder/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ToolTipAndTabOrder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFive = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.rdbOne = new System.Windows.Forms.RadioButton();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.chkTwo = new System.Windows.Forms.CheckBox();
+            this.chkThree = new System.Windows.Forms.CheckBox();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.chkOne = new System.Windows.Forms.CheckBox();
+            this.txtTwo = new System.Windows.Forms.TextBox();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.txtOne = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnFive
+            // 
+            this.btnFive.Location = new System.Drawing.Point(248, 221);
+            this.btnFive.Name = "btnFive";
+            this.btnFive.Size = new System.Drawing.Size(75, 23);
+            this.btnFive.TabIndex = 0;
+            this.btnFive.Text = "Five";
+            this.btnFive.UseVisualStyleBackColor = true;
+            // 
+            // btnFour
+            // 
+            this.btnFour.Location = new System.Drawing.Point(153, 221);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(75, 23);
+            this.btnFour.TabIndex = 1;
+            this.btnFour.Text = "Four";
+            this.btnFour.UseVisualStyleBackColor = true;
+            // 
+            // btnThree
+            // 
+            this.btnThree.Location = new System.Drawing.Point(344, 192);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(75, 23);
+            this.btnThree.TabIndex = 2;
+            this.btnThree.Text = "Three";
+            this.btnThree.UseVisualStyleBackColor = true;
+            // 
+            // rdbOne
+            // 
+            this.rdbOne.AutoSize = true;
+            this.rdbOne.Location = new System.Drawing.Point(100, 61);
+            this.rdbOne.Name = "rdbOne";
+            this.rdbOne.Size = new System.Drawing.Size(71, 17);
+            this.rdbOne.TabIndex = 3;
+            this.rdbOne.TabStop = true;
+            this.rdbOne.Text = "radio One";
+            this.rdbOne.UseVisualStyleBackColor = true;
+            // 
+            // btnSix
+            // 
+            this.btnSix.Location = new System.Drawing.Point(344, 221);
+            this.btnSix.Name = "btnSix";
+            this.btnSix.Size = new System.Drawing.Size(75, 23);
+            this.btnSix.TabIndex = 6;
+            this.btnSix.Text = "Six";
+            this.btnSix.UseVisualStyleBackColor = true;
+            // 
+            // chkTwo
+            // 
+            this.chkTwo.AutoSize = true;
+            this.chkTwo.Location = new System.Drawing.Point(414, 86);
+            this.chkTwo.Name = "chkTwo";
+            this.chkTwo.Size = new System.Drawing.Size(80, 17);
+            this.chkTwo.TabIndex = 7;
+            this.chkTwo.Text = "check Two";
+            this.chkTwo.UseVisualStyleBackColor = true;
+            // 
+            // chkThree
+            // 
+            this.chkThree.AutoSize = true;
+            this.chkThree.Location = new System.Drawing.Point(414, 109);
+            this.chkThree.Name = "chkThree";
+            this.chkThree.Size = new System.Drawing.Size(84, 17);
+            this.chkThree.TabIndex = 8;
+            this.chkThree.Text = "checkThree";
+            this.chkThree.UseVisualStyleBackColor = true;
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Location = new System.Drawing.Point(248, 192);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(75, 23);
+            this.btnTwo.TabIndex = 9;
+            this.btnTwo.Text = "Two";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            // 
+            // chkOne
+            // 
+            this.chkOne.AutoSize = true;
+            this.chkOne.Location = new System.Drawing.Point(414, 58);
+            this.chkOne.Name = "chkOne";
+            this.chkOne.Size = new System.Drawing.Size(79, 17);
+            this.chkOne.TabIndex = 11;
+            this.chkOne.Text = "check One";
+            this.chkOne.UseVisualStyleBackColor = true;
+            // 
+            // txtTwo
+            // 
+            this.txtTwo.Location = new System.Drawing.Point(247, 84);
+            this.txtTwo.Name = "txtTwo";
+            this.txtTwo.Size = new System.Drawing.Size(100, 20);
+            this.txtTwo.TabIndex = 12;
+            // 
+            // btnOne
+            // 
+            this.btnOne.Location = new System.Drawing.Point(153, 192);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(75, 23);
+            this.btnOne.TabIndex = 13;
+            this.btnOne.Text = "One";
+            this.btnOne.UseVisualStyleBackColor = true;
+            // 
+            // txtOne
+            // 
+            this.txtOne.Location = new System.Drawing.Point(247, 58);
+            this.txtOne.Name = "txtOne";
+            this.txtOne.Size = new System.Drawing.Size(100, 20);
+            this.txtOne.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtOne);
+            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.txtTwo);
+            this.Controls.Add(this.chkOne);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.chkThree);
+            this.Controls.Add(this.chkTwo);
+            this.Controls.Add(this.btnSix);
+            this.Controls.Add(this.rdbOne);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnFour);
+            this.Controls.Add(this.btnFive);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFive;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.RadioButton rdbOne;
+        private System.Windows.Forms.Button btnSix;
+        private System.Windows.Forms.CheckBox chkTwo;
+        private System.Windows.Forms.CheckBox chkThree;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.CheckBox chkOne;
+        private System.Windows.Forms.TextBox txtTwo;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.TextBox txtOne;
+    }
+}
+
